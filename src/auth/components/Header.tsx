ff306bec48e46baad261246ec7bd0b753d7c7102
Evhorus <sesbas778@gmail.com>
2024-11-04 11:17:47 -0500

    se arregla el login

--- src/auth/components/Header.tsx
+++ src/auth/components/Header.tsx
@@ -2,28 +2,28 @@ import Logo from "../../assets/logo.svg";
 
 export const Header = () => {
     return (
-        <header className="fixed top-0 w-full bg-gradient-to-r from-indigo-300 via-indigo-100 to-blue-100 shadow-md z-50">
-            <div className="mx-auto max-w-7xl px-4 py-4 flex items-center justify-between">
+        <header className="fixed top-0 w-full bg-slate-900 bg-opacity-40 shadow-lg">
+            <div className="mx-auto max-w-7xl px-4 py-5 flex items-center justify-between">
                 <div className="flex items-center">
                     <img
                         src={Logo}
                         alt="EnergyTop Logo"
                         className="h-8 w-8 mr-3"
                     />
-                    <h1 className="text-2xl font-semibold text-gray-800 tracking-wide">
+                    <h1 className="text-3xl font-semibold text-white tracking-wide">
                         EnergyTop
                     </h1>
                 </div>
                 <nav>
                     <a
                         href="#"
-                        className="text-sm text-gray-700 hover:text-gray-900 ml-6 font-medium"
+                        className="text-xl text-white hover:text-gray-900 ml-6 font-semibold"
                     >
                         Contacto
                     </a>
                     <a
                         href="#"
-                        className="text-sm text-gray-700 hover:text-gray-900 ml-6 font-medium"
+                        className="text-xl text-white hover:text-gray-900 ml-6 font-semibold"
                     >
                         Ayuda
                     </a>
--- src/auth/layout/AuthLayout.tsx
+++ src/auth/layout/AuthLayout.tsx
@@ -1,13 +1,24 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 import { useAppStore } from "../../shared/store/useAppStore";
+import BgImage from "../../assets/bg.jpg";
 
 export const AuthLayout = () => {
     const isAuth = useAppStore((state) => state.isAuth);
     if (isAuth) return <Navigate to="/dashboard/home" />;
     return (
-        <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-white to-indigo-200 flex flex-col">
+        <div className="min-h-screen flex flex-col ">
             <Header />
+            <div
+                className="absolute inset-0"
+                style={{
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${BgImage})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    zIndex: -1, // Asegúrate de que quede detrás
+                }}
+            />
             <div className="flex-1 flex items-center justify-center mt-10">
                 <Outlet />
             </div>
